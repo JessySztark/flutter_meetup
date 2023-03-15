fc48774009a4e13c53ef5385986d11f259a3427d
Jessy Sztark <Jessy.Sztark@etu.univ-savoie.fr>
2023-03-15 18:31:11 +0100

    flutter TD2 meet up

---
+++ flutter_meetup/lib/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_meetup/app_state.dart';
+import 'package:flutter_meetup/auth_action.dart';
+import 'package:flutter_meetup/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'guest_book.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Meetup'),
+      ),
+      body: ListView(
+        children: [
+          Image.asset('assets/header.png'),
+          const IconAndDetail(Icons.calendar_month, '1er Avril'),
+          const IconAndDetail(Icons.location_city, 'Annecy'),
+          Consumer<ApplicationState>(builder: (context, appState, _) {
+            return AuthAction(
+              loggedIn: appState.loggedIn,
+              signOut: () {
+                FirebaseAuth.instance.signOut();
+              },
+            );
+          }),
+          const Header('Discussion'),
+          Consumer<ApplicationState>(builder: (context, appState, _) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  GuestBook(
+                      addMessage: (message) =>
+                          appState.addMessageToGuestBook(message)),
+                ],
+              ],
+            );
+          }),
+          const Header('Au programme'),
+          const Paragraph(
+              "Rejoignez-nous pour une journée pleine de démos, d'échanges, et de pizza !"),
+        ],
+      ),
+    );
+  }
+}
